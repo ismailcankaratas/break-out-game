a5b0f852822bf5c287e40612dca4db944facaf73
Kerem Ünce <Keremunce@hotmail.com>
2023-01-14 17:33:32 +0300

    BrickCollision is created and its working

--- src/components/games/breakout/Board.js
+++ src/components/games/breakout/Board.js
@@ -1,9 +1,10 @@
-import { useEffect, useRef } from "react"
+import { cloneElement, useEffect, useRef } from "react"
 import { BallMovement } from "./BallMovement";
 import data from "../../data";
 import WallCollision from "./util/WallCollision";
 import Brick from "./Brick";
 import Paddle from "./Paddle";
+import BrickCollision from "./util/BrickCollision";
 
 let { paddleProps } = data;
 
@@ -19,9 +20,11 @@ export default function Board() {
             const canvas = canvasRef.current;
             const ctx = canvas.getContext("2d");
 
+            
             canvas.width = window.innerWidth;
             canvas.height = window.innerHeight;
 
+            // Assign Bricks
             let newBrickSet = Brick(2, bricks, canvas, brickObj);
 
             if (newBrickSet && newBrickSet.length > 0) {
@@ -30,13 +33,32 @@ export default function Board() {
 
             ctx.clearRect(0, 0, canvas.width, canvas.height);
 
+            // Display Bircks
             bricks.map((brick) => {
                 return brick.draw(ctx);
             });
 
+            // Top Hareketi
             BallMovement(ctx, ballObj);
 
+            // Top ve duvar sekmesi
             WallCollision(ballObj, canvas);
+
+            // Brick Collision
+            let brickCollision;
+            for (let i = 0; i < bricks.length; i++) {
+                brickCollision = BrickCollision(ballObj, bricks[i]);
+
+                if(brickCollision.hit && !bricks[i].broke) {
+                    if (brickCollision.axis === "X") {
+                        ballObj.dx *= -1;
+                        bricks[i].broke = true;
+                    } else if (brickCollision.axis === "Y"){
+                        ballObj.dy *= -1;
+                        bricks[i].broke = true;
+                    }
+                }
+            }
 
             Paddle(ctx,canvas,paddleProps)
 
