6f4aa505243de4ccf783190761652ac0103c9219
ismailcankaratasss@gmail.com <ismailcankaratasss@gmail.com>
2023-01-14 18:59:49 +0300

    finish

--- src/components/games/breakout/playerStats.js
+++ src/components/games/breakout/playerStats.js
@@ -7,7 +7,7 @@ export default function PlayerStates(ctx, player, canvas) {
     // Lives
     ctx.font = "20px Arial"
     ctx.fillStyle = "red"
-    let gap = 0 ;
+    let gap = 0;
     for (let i = 0; i < player.lives; i++) {
         ctx.fillText("❤️", canvas.width / 2 + gap, 30);
         gap += 30;
