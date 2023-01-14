7ce402b89e42a83d5854d924b95519c08e1e9635
ismailcankaratasss@gmail.com <ismailcankaratasss@gmail.com>
2023-01-14 16:27:15 +0300

    Board and Ball added

---
+++ src/components/data.js
@@ -0,0 +1,29 @@
+export default {
+    ballObj: {
+        x: 50,
+        y: 50,
+        dx: 1,
+        dy: 1,
+        rad: 5,
+        speed: 10,
+    },
+    brickObj: {
+        x: 0.5,
+        y: 50,
+        height: 20,
+        density: 2,
+        colors: ["red", "lightblue"],
+    },
+    player: {
+        name: "Dhaval",
+        lives: 5,
+        score: 0,
+        level: 1,
+    },
+    paddleProps: {
+        height: 20,
+        width: 100,
+        x: 100,
+        color: "orange",
+    },
+};
--- src/components/games/breakout/Board.js
+++ src/components/games/breakout/Board.js
@@ -1,7 +1,40 @@
+import { useEffect, useRef } from "react"
+import { BallMovement } from "./BallMovement";
+import data from "../../data";
+
 export default function Board() {
+    const canvasRef = useRef(null);
+
+    useEffect(() => {
+        function render() {
+            const canvas = canvasRef.current;
+            const ctx = canvas.getContext("2d");
+
+            let { ballObj } = data;
+
+            ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+            BallMovement(ctx, ballObj);
+
+            if (ballObj.y - ballObj.rad <= 0 ||
+                ballObj.y + ballObj.rad >= canvas.height
+            ) {
+                ballObj.dy *= -1;
+            }
+            if (ballObj.x + ballObj.rad >= canvas.width ||
+                ballObj.x - ballObj.rad <= 0
+            ) {
+                ballObj.dx *= -1;
+            }
+
+            requestAnimationFrame(render);
+        }
+        render();
+
+    }, []);
     return (
-        <div>
-            <h1>Board</h1>
+        <div className="game">
+            <canvas id="canvas" ref={canvasRef} />
         </div>
     )
 }
